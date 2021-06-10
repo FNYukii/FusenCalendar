53d4918d69aa49f6c1f4d0b3bec3185826cbaeb8
2200434 <aruvino99@gmail.com>
2021-06-10 14:55:39 +0900

    EditFusenActivity.ktの仮レイアウトを作成した

---
+++ app/src/main/java/com/example/y/fusencalendar/EditFusenActivity.kt
@@ -0,0 +1,14 @@
+package com.example.y.fusencalendar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_edit_fusen.*
+
+class EditFusenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_fusen)
+
+    }
+}
