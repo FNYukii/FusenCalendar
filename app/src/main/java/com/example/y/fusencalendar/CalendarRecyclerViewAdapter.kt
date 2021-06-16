3535cdbea9227faff602fe4e5a117897d24e84d2
YuSan357 <forestgreen14689@gmail.com>
2021-06-16 10:22:02 +0900

    CalendarRecyclerViewAdapterクラスをおおまかに作成した。

---
+++ app/src/main/java/com/example/y/fusencalendar/CalendarRecyclerViewAdapter.kt
@@ -0,0 +1,69 @@
+package com.example.y.fusencalendar
+
+import android.content.Intent
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import io.realm.Realm
+import io.realm.Sort
+import io.realm.kotlin.where
+import kotlinx.android.synthetic.main.one_calendar_cell.view.*
+import kotlinx.android.synthetic.main.one_calendar_cell.view.*
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+class CalendarRecyclerViewAdapter(
+    private val days: Array<LocalDate?>
+): RecyclerView.Adapter<CalendarRecyclerViewAdapter.CustomViewHolder>() {
+
+
+    class CustomViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val dateText: TextView = itemView.dateText
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.one_calendar_cell, parent, false)
+        return CustomViewHolder(view)
+    }
+
+
+    override fun getItemCount(): Int {
+        return days.size //days.sizeは35or42
+    }
+
+
+    override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
+
+        //TextViewに値をセット
+        if(days[position] == null){
+            holder.dateText.text = ""
+        }else{
+            val formatter = DateTimeFormatter.ofPattern("d")
+            holder.dateText.text = days[position]?.format(formatter)
+        }
+
+        //もし本日なら、dateTextを赤色で表示
+        if(days[position] == LocalDate.now()){
+            holder.dateText.setTextColor(Color.RED)
+        }
+
+        //日付がnull以外のセルをタップすると、DailyEventListActivityへ遷移。
+        holder.itemView.setOnClickListener {
+            if(days[position] != null){
+                val intent = Intent(it.context, DailyEventListActivity::class.java)
+//                intent.putExtra("date", 0)
+                it.context.startActivity(intent)
+            }
+        }
+
+    }
+
+
+
+}
