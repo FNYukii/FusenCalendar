a2dfd365c05fd14d9e8c61e314fb9612a555b8d6
YuSan357 <forestgreen14689@gmail.com>
2021-06-17 14:54:16 +0900

    カレンダーの日付セルの高さを少し高くした。

--- app/src/main/java/com/example/y/fusencalendar/CalendarFragment.kt
+++ app/src/main/java/com/example/y/fusencalendar/CalendarFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 import kotlinx.android.synthetic.main.fragment_calendar.*
 
 class CalendarFragment : Fragment() {
@@ -31,6 +32,8 @@ class CalendarFragment : Fragment() {
         calendarPager.adapter =CustomPagerAdapter(this.context as FragmentActivity)
         //calendarPagerの開始ページは、真ん中のページ
         calendarPager.setCurrentItem(pageSize / 2,false)
+
+        calendarPager.orientation = ViewPager2.ORIENTATION_VERTICAL
     }
 
 
