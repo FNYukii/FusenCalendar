d252a3bcb7b8ba5411e20a1a95fd2142b40fd41f
2200434 <aruvino99@gmail.com>
2021-06-17 15:24:10 +0900

    EditFusenActivity.ktの更新と削除に成功した

--- app/src/main/java/com/example/y/fusencalendar/EditFusenActivity.kt
+++ app/src/main/java/com/example/y/fusencalendar/EditFusenActivity.kt
@@ -19,6 +19,19 @@ class EditFusenActivity : AppCompatActivity() {
         setContentView(R.layout.activity_edit_fusen)
         realm = Realm.getDefaultInstance()
 
+        val fusenId = intent.getIntExtra("id",0)
+
+        if(fusenId > 0){
+            val fusen = realm.where<Fusen>()
+                .equalTo("id",fusenId).findFirst()
+            titleEdit.setText(fusen?.title.toString())
+            memoEdit.setText(fusen?.memo.toString())
+            Toast.makeText(applicationContext, "色は${fusen?.colorId.toString()}番です。", Toast.LENGTH_SHORT).show()
+            deleteButton.visibility = View.VISIBLE
+        } else{
+            deleteButton.visibility = View.INVISIBLE
+        }
+
         backButton.setOnClickListener{ v: View? ->
             var title: String = "タイトル"
             var memo: String = "メモ"
@@ -29,19 +42,43 @@ class EditFusenActivity : AppCompatActivity() {
             if(!memoEdit.text.isNullOrEmpty()){
                 memo = memoEdit.text.toString()
             }
-            realm.executeTransaction {
-                val maxId = realm.where<Fusen>().max("id")
-                val nextId = (maxId?.toInt() ?: 0) + 1
-                val Fusen = realm.createObject<Fusen>(nextId)
-                Fusen.title = title
-                Fusen.memo = memo
-                Fusen.colorId = colorId
-            }
 
+            when(fusenId){
+                0 -> {
+                    realm.executeTransaction {
+                        val maxId = realm.where<Fusen>().max("id")
+                        val nextId = (maxId?.toInt() ?: 0) + 1
+                        val Fusen = realm.createObject<Fusen>(nextId)
+                        Fusen.title = title
+                        Fusen.memo = memo
+                        Fusen.colorId = colorId
+                    }
+                }
+                else -> {
+                    realm.executeTransaction {
+                        val Fusen = realm.where<Fusen>()
+                            .equalTo("id",fusenId).findFirst()
+                        Fusen?.title = title
+                        Fusen?.memo = memo
+                        Fusen?.colorId = colorId
+                    }
+                }
+            }
             Toast.makeText(applicationContext, "保存しました", Toast.LENGTH_SHORT).show()
             finish()
         }
+        deleteButton.setOnClickListener(){
+            realm.executeTransaction{
+                val fusen = realm.where<Fusen>()
+                    .equalTo("id",fusenId)
+                    ?.findFirst()
+                    ?.deleteFromRealm()
+            }
+            Toast.makeText(applicationContext, "削除しました", Toast.LENGTH_SHORT).show()
+            finish()
+        }
     }
+
     override fun onDestroy() {
         super.onDestroy()
         realm.close()
