5bb66d302997e6a58cc1043f50e00b26ae7a3f44
Yu357 <forestgreen14689@gmail.com>
2021-07-01 20:05:11 +0900

    カレンダーの日付セル内に、登録されているイベントの小さなイメージが表示されるようになった。

--- app/src/main/java/com/example/y/fusencalendar/CalendarRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/y/fusencalendar/CalendarRecyclerViewAdapter.kt
@@ -6,7 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import io.realm.Realm
+import io.realm.Sort
+import io.realm.kotlin.where
 import kotlinx.android.synthetic.main.one_calendar_cell.view.*
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
@@ -18,6 +22,10 @@ class CalendarRecyclerViewAdapter(
 
     class CustomViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
         val dateText: TextView = itemView.dateText
+        val cardImage01: View = itemView.cardImage01
+        val cardImage02: View = itemView.cardImage02
+        val cardImage03: View = itemView.cardImage03
+        val cardImage04: View = itemView.cardImage04
     }
 
 
@@ -35,6 +43,12 @@ class CalendarRecyclerViewAdapter(
 
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
 
+        //変数を用意
+        val year = days[position]?.year ?: -1
+        val month = days[position]?.monthValue ?: -1
+        val day = days[position]?.dayOfMonth ?: -1
+        val realm = Realm.getDefaultInstance()
+
         //TextViewに値をセット
         if(days[position] == null){
             holder.dateText.text = ""
@@ -43,10 +57,109 @@ class CalendarRecyclerViewAdapter(
             holder.dateText.text = days[position]?.format(formatter)
         }
 
-        //もし本日なら、dateTextを赤色で表示
+        //もし当日が本日なら、dateTextを赤色で表示
         if(days[position] == LocalDate.now()){
             holder.dateText.setTextColor(Color.RED)
         }
+
+        //当日に登録されている全イベントを取得
+        val realmResults = realm.where<Event>()
+            .equalTo("year", year)
+            .and()
+            .equalTo("month", month)
+            .and()
+            .equalTo("date", day)
+            .findAll()
+            .sort("hour", Sort.ASCENDING, "minute", Sort.ASCENDING)
+
+        //当日登録されているイベント数が1以上なら、cardImage01を表示
+        if(realmResults.size >= 1){
+            when(realmResults[0]?.colorId){
+                0 -> {
+                    holder.cardImage01.setBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.green))
+                }
+                1 -> {
+                    holder.cardImage01.setBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.blue))
+                }
+                2 -> {
+                    holder.cardImage01.setBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.orange))
+                }
+                3 -> {
+                    holder.cardImage01.setBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.red))
+                }
+                4 -> {
+                    holder.cardImage01.setBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.purple))
+                }
+            }
+        }
+
+        //当日登録されているイベント数が2以上なら、cardImage02を表示
+        if(realmResults.size >= 2){
+            when(realmResults[1]?.colorId){
+                0 -> {
+                    holder.cardImage02.setBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.green))
+                }
+                1 -> {
+                    holder.cardImage02.setBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.blue))
+                }
+                2 -> {
+                    holder.cardImage02.setBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.orange))
+                }
+                3 -> {
+                    holder.cardImage02.setBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.red))
+                }
+                4 -> {
+                    holder.cardImage02.setBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.purple))
+                }
+            }
+        }
+
+        //当日登録されているイベント数が3以上なら、cardImage03を表示
+        if(realmResults.size >= 3){
+            when(realmResults[2]?.colorId){
+                0 -> {
+                    holder.cardImage03.setBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.green))
+                }
+                1 -> {
+                    holder.cardImage03.setBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.blue))
+                }
+                2 -> {
+                    holder.cardImage03.setBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.orange))
+                }
+                3 -> {
+                    holder.cardImage03.setBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.red))
+                }
+                4 -> {
+                    holder.cardImage03.setBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.purple))
+                }
+            }
+        }
+
+        //当日登録されているイベント数が4以上なら、cardImage04を表示
+        if(realmResults.size >= 4){
+            when(realmResults[3]?.colorId){
+                0 -> {
+                    holder.cardImage04.setBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.green))
+                }
+                1 -> {
+                    holder.cardImage04.setBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.blue))
+                }
+                2 -> {
+                    holder.cardImage04.setBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.orange))
+                }
+                3 -> {
+                    holder.cardImage04.setBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.red))
+                }
+                4 -> {
+                    holder.cardImage04.setBackgroundColor(ContextCompat.getColor(holder.itemView.context, R.color.purple))
+                }
+            }
+        }
+
+
+
+
+
 
         //日付がnull以外のセルをタップすると、DailyEventListActivityへ遷移。
         holder.itemView.setOnClickListener {
@@ -54,9 +167,9 @@ class CalendarRecyclerViewAdapter(
                 val intent = Intent(it.context, DailyEventListActivity::class.java)
 
                 //日付情報をintentに付与
-                intent.putExtra("year", days[position]?.year)
-                intent.putExtra("month", days[position]?.monthValue)
-                intent.putExtra("day", days[position]?.dayOfMonth)
+                intent.putExtra("year", year)
+                intent.putExtra("month", month)
+                intent.putExtra("day", day)
 
                 //intent実行
                 it.context.startActivity(intent)
